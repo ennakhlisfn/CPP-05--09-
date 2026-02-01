b3879a2d0122b6fcfbc8abeeb557e02ee127d22e
sennakhl <sennakhl@c4r10p1.1337.ma>
2026-02-01 16:36:03 +0100

    CPP08/ex02

---
+++ CPP08/ex02/main.cpp
@@ -0,0 +1,96 @@
+#include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+
+#include <iostream>
+#include <list>
+#include "MutantStack.hpp"
+
+int main()
+{
+    std::cout << "--- MutantStack ---" << std::endl;
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << "Top: " << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << "Size: " << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+
+    std::cout << "\n--- std::list Comparison ---" << std::endl;
+    std::list<int> ltest;
+
+    ltest.push_back(5);
+    ltest.push_back(17);
+
+    std::cout << "Back: " << ltest.back() << std::endl;
+
+    ltest.pop_back();
+
+    std::cout << "Size: " << ltest.size() << std::endl;
+
+    ltest.push_back(3);
+    ltest.push_back(5);
+    ltest.push_back(737);
+    ltest.push_back(0);
+
+    std::list<int>::iterator lit = ltest.begin();
+    std::list<int>::iterator lite = ltest.end();
+
+    ++lit;
+    --lit;
+    while (lit != lite)
+    {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+
+    return 0;
+}
+
+// int main()
+// {
+//     MutantStack<int> mstack;
+//     mstack.push(5);
+//     mstack.push(17);
+//     std::cout << mstack.top() << std::endl;
+//     mstack.pop();
+//     std::cout << mstack.size() << std::endl;
+//     mstack.push(3);
+//     mstack.push(5);
+//     mstack.push(737);
+
+//     mstack.push(0);
+//     MutantStack<int>::iterator it = mstack.begin();
+//     MutantStack<int>::iterator ite = mstack.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     std::stack<int> s(mstack);
+//     return 0;
+// }
